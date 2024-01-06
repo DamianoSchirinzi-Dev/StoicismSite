c78974c3a21789478136c70807ad64dedd518a46
Damiano Schirinzi <damianoschirinzi.dev@gmail.com>
2024-01-06 19:25:21 +0000

    Fifth Commit

--- src/script.js
+++ src/script.js
@@ -30,7 +30,7 @@ const stoics = [
       "He who spares the wicked injures the good.",
     ],
     info: "Seneca was born in Corduba in Hispania, and raised in Rome, where he was trained in rhetoric and philosophy. His father was Seneca the Elder, his elder brother was Lucius Junius Gallio Annaeanus, and his nephew was the poet Lucan. In AD 41, Seneca was exiled to the island of Corsica under emperor Claudius,[2] but was allowed to return in 49 to become a tutor to Nero. When Nero became emperor in 54, Seneca became his advisor and, together with the praetorian prefect Sextus Afranius Burrus, provided competent government for the first five years of Nero's reign. Seneca's influence over Nero declined with time, and in 65 Seneca was forced to take his own life for alleged complicity in the Pisonian conspiracy to assassinate Nero, of which he was probably innocent.[3] His stoic and calm suicide has become the subject of numerous paintings.",
-    relativeCameraPos: [36, 0.5, -7],
+    relativeCameraPos: [31, 0.5, -7],
   },
 ];
 
